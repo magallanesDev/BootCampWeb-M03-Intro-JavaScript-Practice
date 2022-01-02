53575f555a2718693a7f169200971dfda1f4edab
Mario Filgueiras <magallanesdeveloper@gmail.com>
2022-01-02 22:10:13 +0100

    Ponemos bien los títulos de todas las rondas

--- index.js
+++ index.js
@@ -11,27 +11,16 @@ console.log('');
 console.log('Equipos que van a participar en el playoff (octavos de final):');
 console.log('');
 teams.shuffle().forEach(element => {console.log(element)});
-console.log('');
-console.log('===== OCTAVOS DE FINAL ====');
-console.log('');
-console.log('===== CUARTOS DE FINAL ====');
-console.log('');
-console.log('===== SEMIFINALES ====');
-console.log('');
-console.log('===== FINAL ====');
-console.log('');
-console.log('');
-console.log('===============================================');
-console.log('¡********** CAMPEÓN DEL MUNDO!');
-console.log('===============================================');
-console.log('');
 
 
 function generateGoals(){
     return Math.floor(Math.random() * 10);
 }
 
+console.log('');
+console.log('');
 console.log('===== OCTAVOS DE FINAL ====');
+console.log('');
 let winnersOf16 = [];
 function playRoundOf16() {
     let goals = [];
@@ -46,13 +35,11 @@ function playRoundOf16() {
             if (goals[i] > goals[i+1]) {
                 winnersOf16.push(teams[i]);
                 console.log(`${teams[i]} pasa de ronda`);
-                console.log(winnersOf16);
                 console.log('');
             }
             if (goals[i] < goals[i+1]) {
                 winnersOf16.push(teams[i+1]);
                 console.log(`${teams[i+1]} pasa de ronda`);
-                console.log(winnersOf16);
                 console.log('');
                 
             }
@@ -64,13 +51,11 @@ function playRoundOf16() {
                 if (goals[i] > goals[i+1]) {
                     winnersOf16.push(teams[i]);
                     console.log(`${teams[i]} pasa de ronda`);
-                    console.log(winnersOf16);
                     console.log('');
                 }
                 if (goals[i] < goals[i+1]) {
                     winnersOf16.push(teams[i+1]);
                     console.log(`${teams[i+1]} pasa de ronda`);
-                    console.log(winnersOf16);
                     console.log('');
                 }
                 if (goals[i] === goals[i+1]) {
@@ -81,20 +66,17 @@ function playRoundOf16() {
                     if (goals[i] > goals[i+1]) {
                         winnersOf16.push(teams[i]);
                         console.log(`${teams[i]} pasa de ronda`);
-                        console.log(winnersOf16);
                         console.log('');
                     }
                     if (goals[i] < goals[i+1]) {
                         winnersOf16.push(teams[i+1]);
                         console.log(`${teams[i+1]} pasa de ronda`);
-                        console.log(winnersOf16);
                         console.log('');
                     }
                     if (goals[i] === goals[i+1]) {
                         console.log('Hubo otro empate en los penaltis. Pasa el equipo local');
                         winnersOf16.push(teams[i]);
                         console.log(`${teams[i]} pasa de ronda`);
-                        console.log(winnersOf16);
                         console.log('');
                     }
                 }
@@ -105,9 +87,12 @@ function playRoundOf16() {
 }
 
 playRoundOf16();
-console.log(winnersOf16);
 
+
+console.log('');
+console.log('');
 console.log('===== CUARTOS DE FINAL ====');
+console.log('');
 let winnersOf8 = [];
 function playRoundOf8() {
     let goals = [];
@@ -122,13 +107,11 @@ function playRoundOf8() {
             if (goals[i] > goals[i+1]) {
                 winnersOf8.push(winnersOf16[i]);
                 console.log(`${winnersOf16[i]} pasa de ronda`);
-                console.log(winnersOf8);
                 console.log('');
             }
             if (goals[i] < goals[i+1]) {
                 winnersOf8.push(winnersOf16[i+1]);
                 console.log(`${winnersOf16[i+1]} pasa de ronda`);
-                console.log(winnersOf8);
                 console.log('');
                 
             }
@@ -140,13 +123,11 @@ function playRoundOf8() {
                 if (goals[i] > goals[i+1]) {
                     winnersOf8.push(winnersOf16[i]);
                     console.log(`${winnersOf16[i]} pasa de ronda`);
-                    console.log(winnersOf8);
                     console.log('');
                 }
                 if (goals[i] < goals[i+1]) {
                     winnersOf8.push(winnersOf16[i+1]);
                     console.log(`${winnersOf16[i+1]} pasa de ronda`);
-                    console.log(winnersOf8);
                     console.log('');
                 }
                 if (goals[i] === goals[i+1]) {
@@ -157,20 +138,17 @@ function playRoundOf8() {
                     if (goals[i] > goals[i+1]) {
                         winnersOf8.push(winnersOf16[i]);
                         console.log(`${winnersOf16[i]} pasa de ronda`);
-                        console.log(winnersOf8);
                         console.log('');
                     }
                     if (goals[i] < goals[i+1]) {
                         winnersOf8.push(winnersOf16[i+1]);
                         console.log(`${winnersOf16[i+1]} pasa de ronda`);
-                        console.log(winnersOf8);
                         console.log('');
                     }
                     if (goals[i] === goals[i+1]) {
                         console.log('Hubo otro empate en los penaltis. Pasa el equipo local');
                         winnersOf8.push(winnersOf16[i]);
                         console.log(`${winnersOf16[i]} pasa de ronda`);
-                        console.log(winnersOf8);
                         console.log('');
                     }
                 }
@@ -181,10 +159,12 @@ function playRoundOf8() {
 }
 
 playRoundOf8();
-console.log(winnersOf8);
 
 
+console.log('');
+console.log('');
 console.log('===== SEMIFINALES ====');
+console.log('');
 let winnersOf4 = [];
 function playRoundOf4() {
     let goals = [];
@@ -199,13 +179,11 @@ function playRoundOf4() {
             if (goals[i] > goals[i+1]) {
                 winnersOf4.push(winnersOf8[i]);
                 console.log(`${winnersOf8[i]} pasa de ronda`);
-                console.log(winnersOf4);
                 console.log('');
             }
             if (goals[i] < goals[i+1]) {
                 winnersOf4.push(winnersOf8[i+1]);
                 console.log(`${winnersOf8[i+1]} pasa de ronda`);
-                console.log(winnersOf4);
                 console.log('');
                 
             }
@@ -217,13 +195,11 @@ function playRoundOf4() {
                 if (goals[i] > goals[i+1]) {
                     winnersOf4.push(winnersOf8[i]);
                     console.log(`${winnersOf8[i]} pasa de ronda`);
-                    console.log(winnersOf4);
                     console.log('');
                 }
                 if (goals[i] < goals[i+1]) {
                     winnersOf4.push(winnersOf8[i+1]);
                     console.log(`${winnersOf8[i+1]} pasa de ronda`);
-                    console.log(winnersOf4);
                     console.log('');
                 }
                 if (goals[i] === goals[i+1]) {
@@ -234,20 +210,17 @@ function playRoundOf4() {
                     if (goals[i] > goals[i+1]) {
                         winnersOf4.push(winnersOf8[i]);
                         console.log(`${winnersOf8[i]} pasa de ronda`);
-                        console.log(winnersOf4);
                         console.log('');
                     }
                     if (goals[i] < goals[i+1]) {
                         winnersOf4.push(winnersOf8[i+1]);
                         console.log(`${winnersOf8[i+1]} pasa de ronda`);
-                        console.log(winnersOf4);
                         console.log('');
                     }
                     if (goals[i] === goals[i+1]) {
                         console.log('Hubo otro empate en los penaltis. Pasa el equipo local');
                         winnersOf4.push(winnersOf8[i]);
                         console.log(`${winnersOf8[i]} pasa de ronda`);
-                        console.log(winnersOf4);
                         console.log('');
                     }
                 }
@@ -258,10 +231,12 @@ function playRoundOf4() {
 }
 
 playRoundOf4();
-console.log(winnersOf4);
 
 
+console.log('');
+console.log('');
 console.log('===== FINAL ====');
+console.log('');
 let winnersOf2 = [];
 function playRoundOf2() {
     let goals = [];
@@ -275,14 +250,19 @@ function playRoundOf2() {
             
             if (goals[i] > goals[i+1]) {
                 winnersOf2.push(winnersOf4[i]);
-                console.log(`${winnersOf4[i]} pasa de ronda`);
-                console.log(winnersOf2);
+                console.log('');
+                console.log('===============================================');
+                console.log(`¡${winnersOf4[i]} CAMPEÓN DEL MUNDO!`);
+                console.log('===============================================');
                 console.log('');
             }
             if (goals[i] < goals[i+1]) {
                 winnersOf2.push(winnersOf4[i+1]);
-                console.log(`${winnersOf4[i+1]} pasa de ronda`);
-                console.log(winnersOf2);
+                console.log('');
+                console.log('===============================================');
+                console.log(`¡${winnersOf4[i+1]} CAMPEÓN DEL MUNDO!`);
+                console.log('===============================================');
+                console.log('');
                 console.log('');
                 
             }
@@ -293,14 +273,18 @@ function playRoundOf2() {
                 console.log(`${winnersOf4[i]} ${goals[i]} - ${winnersOf4[i+1]} ${goals[i+1]}`);
                 if (goals[i] > goals[i+1]) {
                     winnersOf2.push(winnersOf4[i]);
-                    console.log(`${winnersOf4[i]} pasa de ronda`);
-                    console.log(winnersOf2);
+                    console.log('');
+                    console.log('===============================================');
+                    console.log(`¡${winnersOf4[i]} CAMPEÓN DEL MUNDO!`);
+                    console.log('===============================================');
                     console.log('');
                 }
                 if (goals[i] < goals[i+1]) {
                     winnersOf2.push(winnersOf4[i+1]);
-                    console.log(`${winnersOf4[i+1]} pasa de ronda`);
-                    console.log(winnersOf2);
+                    console.log('');
+                    console.log('===============================================');
+                    console.log(`¡${winnersOf4[i+1]} CAMPEÓN DEL MUNDO!`);
+                    console.log('===============================================');
                     console.log('');
                 }
                 if (goals[i] === goals[i+1]) {
@@ -310,21 +294,27 @@ function playRoundOf2() {
                     console.log(`${winnersOf4[i]} ${goals[i]} - ${winnersOf4[i+1]} ${goals[i+1]}`);
                     if (goals[i] > goals[i+1]) {
                         winnersOf2.push(winnersOf4[i]);
-                        console.log(`${winnersOf4[i]} pasa de ronda`);
-                        console.log(winnersOf2);
+                        console.log('');
+                        console.log('===============================================');
+                        console.log(`¡${winnersOf4[i]} CAMPEÓN DEL MUNDO!`);
+                        console.log('===============================================');
                         console.log('');
                     }
                     if (goals[i] < goals[i+1]) {
                         winnersOf2.push(winnersOf4[i+1]);
-                        console.log(`${winnersOf4[i+1]} pasa de ronda`);
-                        console.log(winnersOf2);
+                        console.log('');
+                        console.log('===============================================');
+                        console.log(`¡${winnersOf4[i+1]} CAMPEÓN DEL MUNDO!`);
+                        console.log('===============================================');
                         console.log('');
                     }
                     if (goals[i] === goals[i+1]) {
                         console.log('Hubo otro empate en los penaltis. Pasa el equipo local');
                         winnersOf2.push(winnersOf4[i]);
-                        console.log(`${winnersOf4[i]} pasa de ronda`);
-                        console.log(winnersOf2);
+                        console.log('');
+                        console.log('===============================================');
+                        console.log(`¡${winnersOf4[i]} CAMPEÓN DEL MUNDO!`);
+                        console.log('===============================================');
                         console.log('');
                     }
                 }
@@ -335,6 +325,3 @@ function playRoundOf2() {
 }
 
 playRoundOf2();
-console.log(winnersOf2);
-
-
